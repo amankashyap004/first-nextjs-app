9961860c53a89c8559c5883d12d50377bc630fd1
amankashyap004 <amankashyap004@gmail.com>
2023-07-23 17:33:59 +0530

    create content component

---
+++ src/components/Content.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Content = () => {
+   return (
+      <main className="container my-4 p-4 h-full flex justify-start items-start">
+         <p>This is the main content of the page.</p>
+      </main>
+   );
+};
+
+export default Content;
